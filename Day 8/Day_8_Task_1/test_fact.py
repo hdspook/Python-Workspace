3e4b3aa33b8fe67d7229cf74fa243e9a6294c0f5
hdspook <31802884+hdspook@users.noreply.github.com>
2020-10-15 10:50:15 +0530

    Updated

--- Day 8/Day_8_Task_1/test_fact.py
+++ Day 8/Day_8_Task_1/test_fact.py
@@ -1,12 +1,19 @@
 import math
 from fact import fact
+import pytest
 
 def test_fact():
     num = 25
     assert math.factorial(num) == fact(num)
 
+def test_zero():
+    with pytest.raises(ZeroDivisionError):
+        1/0
+
 def testsquare():
     num = 7
     assert 7*7 == 40
+
+
 
 
