53596501b51b6b84f38c1df967ab75672d35526a
hdspook <31802884+hdspook@users.noreply.github.com>
2020-10-28 20:37:34 +0530

    Updated

--- File Management System (Using Django)/file_management_system/file_management_system/settings.py
+++ File Management System (Using Django)/file_management_system/file_management_system/settings.py
@@ -42,6 +42,7 @@ INSTALLED_APPS = [
     'converter.apps.ConverterConfig',
     'retriever.apps.RetrieverConfig',
     'splitter.apps.SplitterConfig',
+    'rest_framework_swagger',
     'rest_framework',
 ]
 
